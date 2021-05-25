dcc102b07636cf69cf81fcc993e774476b46e414
seohyunsim <ssh4563@naver.com>
2021-05-25 15:59:48 +0900

    fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,12 @@ function login() {
         id : id.value,
         psword : psword.value,
     };
-    console.log(req);
+
+    fetch("/login", {
+      method: "POST",
+      Headers: {
+        "Content-Type" : "application/json",
+      }, 
+      body: JSON.stringify(req)
+    });
 }
